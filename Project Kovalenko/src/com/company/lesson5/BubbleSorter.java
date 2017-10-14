7a077aa35955220da24c56828f2a3f5d88abc338
Elen <Elen@DESKTOP-G4CPDQ0>
2017-10-14 16:52:58 +0300

    Вариант 2: Изменен внутренний цикл for

--- Project Kovalenko/src/com/company/lesson5/BubbleSorter.java
+++ Project Kovalenko/src/com/company/lesson5/BubbleSorter.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class BubbleSorter {
 	public static void main(String[] args) {
-		int[] array = new int[] { 12, 13, 17 };
+		int[] array = new int[] { 12, 13, 17, 5, 50 };
 		bubbleSort(array);
 		System.out.println(Arrays.toString(array));
 	}
 
 	public static void bubbleSort(int[] array) {
 		for (int i = 0; i < array.length; i++) {
-			for (int j = array.length - 1; j > i; j--) {
+			for (int j = 1; j < array.length; j++) {
 				if (array[j - 1] < array[j]) {
 					int tmp = array[j - 1];
 					array[j - 1] = array[j];
