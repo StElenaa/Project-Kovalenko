1cf149f3bb40a95d27324c9297337623a1296083
Elen <Elen@DESKTOP-G4CPDQ0>
2017-10-15 14:03:26 +0300

    Вариант 3: Добавлена возможность дострочного окончания сортировки

--- Project Kovalenko/src/com/company/lesson5/BubbleSorter.java
+++ Project Kovalenko/src/com/company/lesson5/BubbleSorter.java
@@ -4,20 +4,39 @@ import java.util.Arrays;
 
 public class BubbleSorter {
 	public static void main(String[] args) {
-		int[] array = new int[] { 12, 13, 17, 5, 50 };
+		int[] array = new int[] { 5, 2, 3, 1, 4 };
+		System.out.println(Arrays.toString(array));
 		bubbleSort(array);
 		System.out.println(Arrays.toString(array));
 	}
 
+	/**
+	 * Метод сортирует заданный массив от большего к меньшему значению элементов.
+	 * Предусмотрено досрочное окончания сортировки.
+	 * 
+	 * @param array
+	 *            - сортируемый массив
+	 */
 	public static void bubbleSort(int[] array) {
-		for (int i = 0; i < array.length; i++) {
+		boolean fin = false;
+		int maxCount = array.length * (array.length - 1);
+		int count = 0;
+		for (int i = 0; i < array.length && !fin; i++) {
+			fin = true;
 			for (int j = 1; j < array.length; j++) {
+				count++;
 				if (array[j - 1] < array[j]) {
+					fin = false;
 					int tmp = array[j - 1];
 					array[j - 1] = array[j];
 					array[j] = tmp;
 				}
 			}
+		}
+		System.out.println("Максимальное количество циклов - " + maxCount);
+		System.out.println("Реальное количество циклов - " + count);
+		if (maxCount > count) {
+			System.out.println("Досрочное окончание сортировки");
 		}
 	}
 }
