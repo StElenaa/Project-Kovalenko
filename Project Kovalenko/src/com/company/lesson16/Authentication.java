0f62ee7f3d00765638b9d6dc652099ba68ddb723
Elen <Elen@DESKTOP-G4CPDQ0>
2017-11-27 16:38:21 +0200

    Исправлен regex

--- Project Kovalenko/src/com/company/lesson16/Authentication.java
+++ Project Kovalenko/src/com/company/lesson16/Authentication.java
@@ -89,10 +89,10 @@ public class Authentication {
 	public static boolean isCorrect(String login, String password, String confPassword)
 			throws WrongLoginException, WrongPasswordException {
 		System.out.println(login + " " + password + " " + confPassword);
-		if (login == null || login.length() > 20 || !login.matches("[a-z0-9_-]")) {
+		if (login == null || login.length() > 20 || !login.matches("[A-Za-z0-9_]\\w+")) {
 			throw new WrongLoginException("Неверный логин");
 		}
-		if (password == null || password.length() > 20 || !password.matches("[a-z0-9_-]")
+		if (password == null || password.length() > 20 || !password.matches("[A-Za-z0-9_]\\w+")
 				|| !password.equals(confPassword)) {
 			throw new WrongPasswordException("Неверный пароль");
 		}
