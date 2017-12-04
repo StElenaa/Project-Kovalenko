52f76d34a23e217224765f712e3f3f4e66cdb4f3
Elen <Elen@DESKTOP-G4CPDQ0>
2017-12-04 16:03:25 +0200

    Lesson 18 - NewThread

---
+++ Project Kovalenko/src/com/company/lesson18/NewThread.java
@@ -0,0 +1,19 @@
+package com.company.lesson18;
+
+public class NewThread extends Thread {
+	@Override
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			System.out.println("A " + Thread.currentThread().getName());
+		}
+	}
+
+	public static void main(String[] args) {
+		NewThread nt1 = new NewThread();
+		NewThread nt2 = new NewThread();
+		NewThread nt3 = new NewThread();
+		nt1.start();
+		nt2.start();
+		nt3.start();
+	}
+}
